0883e11e4d136f5cc4aca83af59d78d870b29ffc
Andile <marotyamandile@gmail.com>
2024-04-23 09:56:43 +0200

    Education and profile responsiveness done

--- src/pages/Education.jsx
+++ src/pages/Education.jsx
@@ -5,38 +5,30 @@ const Education = () => {
   return (
     <div className='bg-black min-h-56 flex flex-col justify-center items-center text-white text-[Inter]'>
       <hr className='bg-white w-[90%] mt-5'/>
-      <h1 className='text-gold  w-[84%] flex justify-end'><SchoolIcon /> Education</h1>
+      <h1 className='text-gold w-[90%] sm:w-84% flex justify-end px-4'><SchoolIcon /> Education</h1>
 
-      {/*Education body*/}
-      <div className='flex w-[88%]'>
-        <div>
+      {/* Education body */}
+      <div className='flex flex-col sm:flex-row w-[90%] sm:w-88% px-4'>
+        <div className='mb-4 sm:mb-0 sm:mr-4'>
           <h3 className='font-bold'>University of the Western Cape</h3>
           <p>Cape Town</p>
           <span>2019 - 2022</span>
-
         </div>
-        <div class="timeline">
-          <div class="relative">
-            <div class="dot">
-
-            </div>
-            <div class="pl-10">
-              <span class="timeline-course">BSc Computer Science</span>
-              
-              <ul class="list-disc list-inside">
-
-                <li >
+        <div className='timeline flex-1'>
+          <div className='relative'>
+            <div className='dot'></div>
+            <div className='pl-10'>
+              <span className='timeline-course'>BSc Computer Science</span>
+              <ul className='list-disc list-inside'>
+                <li>
                   Java, Python, C, Assembly,
-                  Data Structures &
-                  Algorithms, Networking,
-                  Machine Learning, OS,
-                  Human-Computer
-                  Interaction, Software
-                  Engineering, Databases,
-                  Data Analysis, Modern
-                  Algebra, Real Analysis,
-                  Calculus, Advanced Calculus, Physical Science.
-                
+                  Data Structures & Algorithms,
+                  Networking, Machine Learning,
+                  OS, Human-Computer Interaction,
+                  Software Engineering, Databases,
+                  Data Analysis, Modern Algebra,
+                  Real Analysis, Calculus,
+                  Advanced Calculus, Physical Science.
                 </li>
               </ul>
             </div>
@@ -44,7 +36,7 @@ const Education = () => {
         </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Education
+export default Education;
--- src/pages/profile.jsx
+++ src/pages/profile.jsx
@@ -5,30 +5,31 @@ import LocationOnIcon from '@mui/icons-material/LocationOn';
 
 const Profile = () => {
   return (
-    <div className='bg-black min-h-40 flex flex-col justify-center items-center text-white text-[Inter]'>
-      <div className='bg-black text-white flex justify-between items-center mb-4  w-[90%] p-4'>
-        <div>
-          <a><PhoneIphoneIcon/>+27 63 591 1462</a><br/>
-          <a><EmailIcon/>marotyamandile@gmail.com</a>
-          <p><LocationOnIcon/>Port Elizabeth, South Africa</p>
-        </div>
-        <div className='flex'>
-          <div className='mr-2'>
-            <h1>Mandile Marotya</h1>
-            <p>Fullstack Software Engineer</p>
-            <p>BSc Computer Scientist</p>
-            
+    <div className='bg-black min-h-screen flex flex-col justify-center items-center text-white text-[Inter]'>
+      <div className='bg-black text-white flex flex-col sm:flex-row justify-between items-center mb-4 w-full sm:w-5/6 p-4'>
+        <div className='mb-4 sm:mb-0 sm:mr-4'>
+          <h1 className='text-2xl sm:text-3xl font-bold'>Mandile Marotya</h1>
+          <p className='text-lg sm:text-xl'>Fullstack Software Engineer</p>
+          <p className='text-lg sm:text-xl'>BSc Computer Scientist</p>
+          <div className='mt-2'>
+            <a className='flex items-center mb-2'><PhoneIphoneIcon className='mr-2' />+27 63 591 1462</a>
+            <a className='flex items-center mb-2'><EmailIcon className='mr-2' />marotyamandile@gmail.com</a>
+            <p className='flex items-center'><LocationOnIcon className='mr-2' />Port Elizabeth, South Africa</p>
           </div>
-          <img src={`${process.env.PUBLIC_URL}/images/profile-pic.JPG`} alt='Profile Pic' className='w-20 h-22 rounded-lg' />
         </div>
+        <img
+          src={`${process.env.PUBLIC_URL}/images/profile-pic.JPG`}
+          alt='Profile Pic'
+          className='w-32 h-32 sm:w-40 sm:h-40 rounded-lg'
+        />
       </div>
-      <hr className='bg-white w-[90%]' />
-      <div className='p-4 mb-8'>
-        <h2 className='text-gold'>About Me</h2>
-        <p>
-          	I'm highly peer and self-driven Computer Scientist/ Fullstack engineer seeking full-time employment in
-            the Software Engineering industry.<br/> I am team player, I have impressive good problem solving skills,
-            communication skills, data analysis and visualization skills. 
+      <hr className='bg-white w-full sm:w-5/6' />
+      <div className='p-4 mb-8 w-full sm:w-5/6'>
+        <h2 className='text-gold text-xl sm:text-2xl'>About Me</h2>
+        <p className='text-lg sm:text-xl'>
+          I'm a highly peer and self-driven Computer Scientist/Fullstack engineer seeking full-time employment in
+          the Software Engineering industry. I am a team player, possessing impressive problem-solving,
+          communication, data analysis, and visualization skills.
         </p>
       </div>
     </div>

284c942499185e7a762709dcedcfe1907107163a
Andile <marotyamandile@gmail.com>
2024-10-16 05:23:41 +0200

    Profile section editted

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -8,56 +8,6 @@ const Projects = () => {
 
       {/* Body */}
       <div className='mt-6 w-[90%] max-w-screen-lg ml-4 mb-8'>
-        {/* Full Stack */}
-        <h1 className='text-gold mb-4'>Full Stack:</h1>
-        <div className='flex justify-between mb-8'>
-          {/* Store admin */}
-          <div className='project-item bg-[#2B2924] border border-black rounded-xl p-3 w-[45%]'>
-            <img src={`${process.env.PUBLIC_URL}/images/admin.png`} alt='Store admin' className='w-full h-auto mx-auto' />
-            <p className='my-5 text-center'>Full stack Store admin app, using React, Node.js, and MongoDB.</p>
-            <a href='https://msstoreadmin-frontend.onrender.com/' target='_blank' rel='noopener noreferrer'>
-              <button className='block mx-auto bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded'>View</button>
-            </a>
-          </div>
-          {/* CRUD app */}
-          <div className='project-item bg-[#2B2924] border border-black rounded-xl p-3 w-[45%]'>
-            <img src={`${process.env.PUBLIC_URL}/images/crud.png`} alt='CRUD app' className='w-full h-auto mx-auto' />
-            <p className='my-5 text-center'>Full stack CRUD app, using React, Spring Boot, and MySQL.</p>
-            <a href='https://msstoreadmin-frontend.onrender.com/' target='_blank' rel='noopener noreferrer'>
-              <button className='block mx-auto bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded'>View</button>
-            </a>
-          </div>
-        </div>
-
-        {/* Frontend */}
-        <h1 className='text-gold my-8'>Frontend:</h1>
-        <div className='flex justify-between mb-8'>
-          {/* Blockchain */}
-          <div className='project-item bg-[#2B2924] border border-black rounded-xl p-3 w-[30%]'>
-            <img src={`${process.env.PUBLIC_URL}/images/blockchain.png`} alt='Blockchain app' className='w-full h-auto mx-auto' />
-            <p className='my-5 text-center'>Blockchain application using React and Node.js.</p>
-            <a href='https://ms-blockchain.netlify.app/' target='_blank' rel='noopener noreferrer'>
-              <button className='block mx-auto bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded'>View</button>
-            </a>
-          </div>
-          {/* Restaurant */}
-          <div className='project-item bg-[#2B2924] border border-black rounded-xl p-3 w-[30%]'>
-            <img src={`${process.env.PUBLIC_URL}/images/myira.png`} alt='Restaurant app' className='w-full h-auto mx-auto' />
-            <p className='my-5 text-center'>Restaurant app using React and Spring Boot.</p>
-            <a href='https://myira-restaurant.netlify.app/' target='_blank' rel='noopener noreferrer'>
-              <button className='block mx-auto bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded'>View</button>
-            </a>
-          </div>
-          {/* Panel Beating */}
-          <div className='project-item bg-[#2B2924] border border-black rounded-xl p-3 w-[30%]'>
-            <img src={`${process.env.PUBLIC_URL}/images/dumza.png`} alt='Panel Beating app' className='w-full h-auto mx-auto' />
-            <p className='my-5 text-center'>Panel Beating app using React and Spring Boot.</p>
-            <a href='https://dumzapanelb.onrender.com/' target='_blank' rel='noopener noreferrer'>
-              <button className='block mx-auto bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded'>View</button>
-            </a>
-          </div>
-        </div>
-
         {/* Games */}
         <h1 className='text-gold my-8'>Games:</h1>
         <div className='flex justify-between mb-8'>
@@ -123,6 +73,58 @@ const Projects = () => {
             </a>
           </div>
         </div>
+
+        {/* Full Stack */}
+        <h1 className='text-gold mb-4'>Full Stack:</h1>
+        <div className='flex justify-between mb-8'>
+          {/* Store admin */}
+          <div className='project-item bg-[#2B2924] border border-black rounded-xl p-3 w-[45%]'>
+            <img src={`${process.env.PUBLIC_URL}/images/admin.png`} alt='Store admin' className='w-full h-auto mx-auto' />
+            <p className='my-5 text-center'>Full stack Store admin app, using React, Node.js, and MongoDB.</p>
+            <a href='https://msstoreadmin-frontend.onrender.com/' target='_blank' rel='noopener noreferrer'>
+              <button className='block mx-auto bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded'>View</button>
+            </a>
+          </div>
+          {/* CRUD app */}
+          <div className='project-item bg-[#2B2924] border border-black rounded-xl p-3 w-[45%]'>
+            <img src={`${process.env.PUBLIC_URL}/images/crud.png`} alt='CRUD app' className='w-full h-auto mx-auto' />
+            <p className='my-5 text-center'>Full stack CRUD app, using React, Spring Boot, and MySQL.</p>
+            <a href='https://msstoreadmin-frontend.onrender.com/' target='_blank' rel='noopener noreferrer'>
+              <button className='block mx-auto bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded'>View</button>
+            </a>
+          </div>
+        </div>
+
+        {/* Frontend */}
+        <h1 className='text-gold my-8'>Frontend:</h1>
+        <div className='flex justify-between mb-8'>
+          {/* Blockchain */}
+          <div className='project-item bg-[#2B2924] border border-black rounded-xl p-3 w-[30%]'>
+            <img src={`${process.env.PUBLIC_URL}/images/blockchain.png`} alt='Blockchain app' className='w-full h-auto mx-auto' />
+            <p className='my-5 text-center'>Blockchain application using React and Node.js.</p>
+            <a href='https://ms-blockchain.netlify.app/' target='_blank' rel='noopener noreferrer'>
+              <button className='block mx-auto bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded'>View</button>
+            </a>
+          </div>
+          {/* Restaurant */}
+          <div className='project-item bg-[#2B2924] border border-black rounded-xl p-3 w-[30%]'>
+            <img src={`${process.env.PUBLIC_URL}/images/myira.png`} alt='Restaurant app' className='w-full h-auto mx-auto' />
+            <p className='my-5 text-center'>Restaurant app using React and Spring Boot.</p>
+            <a href='https://myira-restaurant.netlify.app/' target='_blank' rel='noopener noreferrer'>
+              <button className='block mx-auto bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded'>View</button>
+            </a>
+          </div>
+          {/* Panel Beating */}
+          <div className='project-item bg-[#2B2924] border border-black rounded-xl p-3 w-[30%]'>
+            <img src={`${process.env.PUBLIC_URL}/images/dumza.png`} alt='Panel Beating app' className='w-full h-auto mx-auto' />
+            <p className='my-5 text-center'>Panel Beating app using React and Spring Boot.</p>
+            <a href='https://dumzapanelb.onrender.com/' target='_blank' rel='noopener noreferrer'>
+              <button className='block mx-auto bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded'>View</button>
+            </a>
+          </div>
+        </div>
+
+        
       </div>
     </div>
   );
--- src/pages/profile.jsx
+++ src/pages/profile.jsx
@@ -9,7 +9,7 @@ const Profile = () => {
       <div className='bg-black text-white flex flex-col sm:flex-row justify-between items-center mb-4 w-full sm:w-5/6 p-4'>
         <div className='mb-4 sm:mb-0 sm:mr-4'>
           <h1 className='text-2xl sm:text-3xl font-bold'>Mandile Marotya</h1>
-          <p className='text-lg sm:text-xl'>Fullstack Software Engineer</p>
+          <p className='text-lg sm:text-xl'>Fullstack Game Programmer</p>
           <p className='text-lg sm:text-xl'>BSc Computer Scientist</p>
           <div className='mt-2'>
             <a className='flex items-center mb-2'><PhoneIphoneIcon className='mr-2' />+27 63 591 1462</a>
@@ -27,8 +27,8 @@ const Profile = () => {
       <div className='p-4 mb-8 w-full sm:w-5/6'>
         <h2 className='text-gold text-xl sm:text-2xl'>About Me</h2>
         <p className='text-lg sm:text-xl'>
-          I'm a highly peer and self-driven Computer Scientist/Fullstack engineer seeking full-time employment in
-          the Software Engineering industry. I am a team player, possessing impressive problem-solving,
+          I'm a highly peer and self-driven Fullstack Game Developer seeking full-time employment in
+          the Game Development industry. I am a team player, possessing impressive problem-solving,
           communication, data analysis, and visualization skills.
         </p>
       </div>

8026e87901ddbe452f97474d3d69a555308ae637
Andile <marotyamandile@gmail.com>
2025-08-21 16:06:15 +0200

    navbar routing fixed

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,9 +1,8 @@
-import React, { useState } from 'react';
-import { Link } from 'react-scroll';
+import React, { useState } from "react";
+import { HashLink } from "react-router-hash-link";
 
 const Navbar = () => {
   const [isOpen, setIsOpen] = useState(false);
-  
 
   const toggleMenu = () => {
     setIsOpen(!isOpen);
@@ -13,7 +12,7 @@ const Navbar = () => {
     <nav className="bg-gray-900 sticky top-0 z-50">
       <div className="max-w-7xl mx-auto px-4">
         <div className="flex justify-center h-16 items-center">
-          {/* Menu button (hamburger icon) */}
+          {/* Menu button (hamburger) */}
           <div className="flex items-center">
             <div className="block lg:hidden">
               <button
@@ -25,67 +24,70 @@ const Navbar = () => {
                   viewBox="0 0 24 24"
                   xmlns="http://www.w3.org/2000/svg"
                 >
-                  {isOpen ? (
-                    <path
-                      fillRule="evenodd"
-                      clipRule="evenodd"
-                      d="M4 5h16a1 1 0 0 1 0 2H4a1 1 0 1 1 0-2zm0 6h16a1 1 0 0 1 0 2H4a1 1 0 1 1 0-2zm0 6h16a1 1 0 0 1 0 2H4a1 1 0 1 1 0-2z"
-                    />
-                  ) : (
-                    <path
-                      fillRule="evenodd"
-                      clipRule="evenodd"
-                      d="M4 5h16a1 1 0 0 1 0 2H4a1 1 0 1 1 0-2zm0 6h16a1 1 0 0 1 0 2H4a1 1 0 0 1 0-2zm0 6h16a1 1 0 0 1 0 2H4a1 1 0 1 1 0-2z"
-                    />
-                  )}
+                  <path
+                    fillRule="evenodd"
+                    clipRule="evenodd"
+                    d="M4 5h16a1 1 0 0 1 0 2H4a1 1 0 1 1 0-2zm0 6h16a1 1 0 0 1 0 2H4a1 1 0 1 1 0-2zm0 6h16a1 1 0 0 1 0 2H4a1 1 0 1 1 0-2z"
+                  />
                 </svg>
               </button>
             </div>
           </div>
+
           {/* Menu items */}
-          <div className={`lg:flex lg:items-center lg:space-x-4 ${isOpen ? 'block' : 'hidden'} w-full lg:w-auto justify-center`}>
-            <a
-              href="#profile"
+          <div
+            className={`lg:flex lg:items-center lg:space-x-4 ${
+              isOpen ? "block" : "hidden"
+            } w-full lg:w-auto justify-center`}
+          >
+            <HashLink
+              smooth
+              to="/#profile"
               className="text-gray-300 hover:text-gold hover:border-b-2 cursor-pointer block py-2"
               onClick={toggleMenu}
             >
               Home
-            </a>
-            <a
-              href="#education"
+            </HashLink>
+            <HashLink
+              smooth
+              to="/#education"
               className="text-gray-300 hover:text-gold hover:border-b-2 cursor-pointer block py-2"
               onClick={toggleMenu}
             >
               Education
-            </a>
-            <a
-              href="#experience"
+            </HashLink>
+            <HashLink
+              smooth
+              to="/#experience"
               className="text-gray-300 hover:text-gold hover:border-b-2 cursor-pointer block py-2"
               onClick={toggleMenu}
             >
               Experience
-            </a>
-            <a
-              href="#skills"
+            </HashLink>
+            <HashLink
+              smooth
+              to="/#skills"
               className="text-gray-300 hover:text-gold hover:border-b-2 cursor-pointer block py-2"
               onClick={toggleMenu}
             >
               Skills
-            </a>
-            <a
-              href="#projects"
+            </HashLink>
+            <HashLink
+              smooth
+              to="/#projects"
               className="text-gray-300 hover:text-gold hover:border-b-2 cursor-pointer block py-2"
               onClick={toggleMenu}
             >
               Projects
-            </a>
-            <a
-              href="#contact"
+            </HashLink>
+            <HashLink
+              smooth
+              to="/#contact"
               className="text-gray-300 hover:text-gold hover:border-b-2 cursor-pointer block py-2"
               onClick={toggleMenu}
             >
               Contact
-            </a>
+            </HashLink>
           </div>
         </div>
       </div>
